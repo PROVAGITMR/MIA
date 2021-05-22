8878ead76bb77f2fdb90b0e1fd2fdad6cb2708c4
Monica Ronco <monica.ronco@prometeia.com>
2021-05-22 15:47:13 +0200

    VerificareIgnore

--- EsercitazioneGIT.R
+++ EsercitazioneGIT.R
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 somma = function (a,b){
   somma=a+b
   return(somma)
 }
 
 cat(somma(1,2))
-=======
-somma = function (a,b){
-  somma=a+b
-  return(somma)
-}
 
-cat(somma(1,2))
->>>>>>> test
